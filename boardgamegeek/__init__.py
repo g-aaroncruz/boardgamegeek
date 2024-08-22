046d1a74343bf898288d3e19f07e31e8b5691721
g-aaroncruz <g.aaroncruz@gmail.com>
2024-08-21 18:26:42 -0700

    Update __init__.py with standards & Python3 expressions

--- boardgamegeek/__init__.py
+++ boardgamegeek/__init__.py
@@ -1,23 +1,30 @@
-# coding: utf-8
 """
-.. module:: boardgamegeek
-   :platform: Unix, Windows
-   :synopsis: interface to boardgamegeek.com
+Module providing an interface to boardgamegeek.com
+Platform:
+    - Unix
+    - Windows
 
-.. moduleauthor:: Cosmin Luță <q4break@gmail.com>
+Author:
+    Aaron Cruz <g.aaroncruz@gmail.com>
+    Cosmin Luță <q4break@gmail.com>
 """
 
-from .api import BGGClient, BGGChoose, BGGRestrictDomainTo, BGGRestrictPlaysTo, BGGRestrictSearchResultsTo, BGGRestrictCollectionTo
+from .api import BGGClient, BGGChoose, BGGRestrictDomainTo, \
+    BGGRestrictPlaysTo, BGGRestrictSearchResultsTo, BGGRestrictCollectionTo
 from .legacy_api import BGGClientLegacy
-from .exceptions import BGGError, BGGApiRetryError, BGGApiError, BGGApiTimeoutError, BGGValueError, BGGItemNotFoundError
+from .exceptions import BGGError, BGGApiRetryError, BGGApiError, \
+    BGGApiTimeoutError, BGGValueError, BGGItemNotFoundError
 from .cache import CacheBackendNone, CacheBackendMemory, CacheBackendSqlite
 from .version import __version__
 
-__all__ = ["BGGClient", "BGGChoose", "BGGRestrictSearchResultsTo", "BGGRestrictPlaysTo", "BGGRestrictDomainTo",
-           "BGGRestrictCollectionTo", "BGGError", "BGGValueError", "BGGApiRetryError", "BGGApiError",
-           "BGGApiTimeoutError", "BGGItemNotFoundError", "CacheBackendNone", "CacheBackendSqlite", "CacheBackendMemory"]
+__all__ = ["BGGClient", "BGGChoose", "BGGRestrictSearchResultsTo",
+           "BGGRestrictPlaysTo", "BGGRestrictDomainTo",
+           "BGGRestrictCollectionTo", "BGGError", "BGGValueError",
+           "BGGApiRetryError", "BGGApiError",
+           "BGGApiTimeoutError", "BGGItemNotFoundError", "CacheBackendNone",
+           "CacheBackendSqlite", "CacheBackendMemory"]
 __all__.extend(["BGGClientLegacy"])
 
-__import__('pkg_resources').declare_namespace(__name__)
-
+import importlib
+importlib.import_module(__name__)
 
